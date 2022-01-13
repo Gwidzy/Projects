5818b0d696d7c46d4257e086e4992d922d678654
Guido Schepers <91836824+Gwidzy@users.noreply.github.com>
2022-01-13 10:30:42 +0200

    Update Login.java

--- ChopGUI System/System/src/Login.java
+++ ChopGUI System/System/src/Login.java
@@ -4,16 +4,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author trist
- */
 public class Login extends javax.swing.JFrame {
 
     public Login() {
