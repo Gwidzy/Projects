7bb077a35439d9c21fb0383770d7514e2093c431
Guido <guido@Admins-Air-2.localdomain>
2022-03-04 14:20:01 +0200

    Removed white space

--- ChopGUI System/Database/chopGui.sql
+++ ChopGUI System/Database/chopGui.sql
@@ -91,5 +91,5 @@ WHERE Employee.empID = 1;
 UPDATE Project 
 SET projName =  "Test", projStartDate = "2021-07-08", projEndDate = "2021-07-09", clientName = "test",clientContact="test", clientEmail="test@test.com", empID =1
 WHERE projID = 1;
-
 SELECT * FROM Project;
+
